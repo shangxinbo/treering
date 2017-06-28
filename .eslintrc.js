98ad9ecbd3b88503a05a1d1cbe596d52fde0c88b
shangxinbo <314911714@qq.com>
2017-06-28 11:06:17 +0800

    eslint 还用recomend

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es6: true
+  },
+  parserOptions: {
+    "ecmaVersion": 6,
+    "impliedStrict": true
+  },
+  extends: 'eslint:recommended',
+  // add your custom rules here
+  'rules': {
+    'no-implied-eval': 1,
+    'no-loop-func': 1,
+    'indent': ["warn", 4, { "SwitchCase": 1 }],
+    'no-mixed-spaces-and-tabs': 1,
+    'semi': ["error", "never"],
+    'no-console': 0,
+    'no-unused-vars': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+}
